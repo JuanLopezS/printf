2cb8ad23c95a34929bae47594807d74b50f1e977
Ubuntu <jclopezs1@libertadores.edu.co>
2019-11-10 19:28:38 +0000

    [Feature] Add Files Project Printf -Validator

--- validator.c
+++ validator.c
@@ -15,15 +15,14 @@ int validator(const char *format, va_list print1, MyPrint *ops1)
 
 	while (format && format[i])
 	{
-		if (format[i] == '%' && format[i + 1] == '\0')
-			return (-1);
-		if (format[i] == '%' && (format[i + 1] == ' '
-			 || format[i + 1] != '%'))
+		if (format[i] == '%' && format[i + 1] != '%')
 		{
+			if (format[i + 1] == '\0')
+				return (-1);
 			if (format[i + 1] == ' ')
-			{
+		        {
 				while (format[i + 1] == ' ')
-				i++;
+						i++;
 			}
 			while (j < number)
 			{
@@ -37,15 +36,13 @@ int validator(const char *format, va_list print1, MyPrint *ops1)
 			if (j == number)
 				count += _putchar('%');
 		}
-		else if (format[i] == '%' && format[i + 1] == '%')
-		{
-			count += _putchar('%');
-			i++;
-		}
+		else if (format [i] == '%' && format[i + 1] == '%')
+			{
+				count += _putchar('%');
+				i++;
+			}
 		else
-		{
 			count += _putchar(format[i]);
-		}
 		i++, j = 0;
 	}
 	return (count);
